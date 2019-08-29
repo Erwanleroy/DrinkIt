7a4a452440b900e60e4a3807bf059ddf14d22692
Erwanleroy <erwanlr56@gmail.com>
2019-08-29 15:03:14 +0200

    Add the Player list in the game + jeu de cartes

--- Components/Palmier/theGame.js
+++ Components/Palmier/theGame.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
+
+const NUMEROCARTES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+const FORMESCARTES = ["trefle", "pique", "coeur", "carreau"]
 
 class App extends React.Component {
 
@@ -8,6 +11,10 @@ class App extends React.Component {
         this.state = {
             playerNames: [],
             playerColors: [],
+            whoPlay: 0,
+            cartesRestantes: [],
+            cartesTirees: [],
+            carteActuelle: [],
         }
     }
 
@@ -21,31 +28,87 @@ class App extends React.Component {
         } else {
             alert("There is a problem with datas, the game cant begin")
         }
+        for (let i = 0; i < NUMEROCARTES.length; i++) {
+            for (let j = 0; j < FORMESCARTES.length; j++) {
+                this.state.cartesRestantes.push([NUMEROCARTES[i], FORMESCARTES[j]])
+            }
+        }
     }
 
-    printProps = () => {
-        console.log(this.state)
+    randomInt = (e) => Math.floor(Math.random() * Math.floor(e));
+
+    pickRandomCard = () => {
+        if (this.state.cartesRestantes.length !== 0) {
+            var idCard = this.randomInt(this.state.cartesRestantes.length)
+            var carteActuelle = this.state.cartesRestantes[idCard]
+            this.state.cartesRestantes.splice(idCard, 1)
+            this.state.cartesTirees.push(carteActuelle)
+            this.setState({ carteActuelle: carteActuelle })
+        } else {
+            alert("Plus de cartes deso pas deso")
+        }
+    }
+
+    play = () => {
+        if (this.state.whoPlay === this.state.playerNames.length - 1) {
+            this.setState({ whoPlay: 0 })
+        } else {
+            this.setState({ whoPlay: this.state.whoPlay + 1 })
+        }
+        this.pickRandomCard()
     }
 
     render() {
         return (
             <View>
-                {this.state.playerNames ?
-                    this.state.playerNames.map((value, index) => {
-                        return (
-                            <View key={index}>
-                                <Text>{this.state.playerColors[index]}</Text>
-                            </View>
-                        )
-                    })
-                    : null}
-                <Text onPress={this.printProps}>LE PALMIER THE GAME</Text>
+
+                {/* print All players with them color */}
+                <ScrollView
+                    style={{
+                        flexDirection: "row",
+                    }}
+                    horizontal={true}
+                >
+                    {this.state.playerNames ?
+                        this.state.playerNames.map((value, index) => {
+                            return (
+                                <View
+                                    key={index}
+                                    style={{
+                                        margin: 10,
+                                        overflow: "hidden",
+                                        justifyContent: "center",
+                                        borderRadius: 50,
+                                        width: 50,
+                                        height: 50,
+                                        backgroundColor: this.state.playerColors[index],
+                                    }}
+                                >
+                                    {this.state.playerColors[index] === "black" || this.state.playerColors[index] === "purple" || this.state.playerColors[index] === "purple" || this.state.playerColors[index] === "navy" || this.state.playerColors[index] === "blue" ? (
+                                        <Text style={{ textAlign: "center", fontSize: 20, color: "white" }}
+                                            onPress={() => { alert(value) }}
+                                        >
+                                            {value[0]}
+                                        </Text>
+                                    ) : (
+                                            <Text style={{ textAlign: "center", fontSize: 20 }}
+                                                onPress={() => { alert("Pseudo : " + value) }}
+                                            >
+                                                {value[0]}
+                                            </Text>
+                                        )}
+                                </View>
+                            )
+                        })
+                        : null}
+                </ScrollView>
+                <Button title="Play" onPress={this.play} />
+                <Text style={{ fontSize: 20, textAlign: "center", color: this.state.playerColors[this.state.whoPlay] }}>{this.state.playerNames[this.state.whoPlay]}</Text>
+                <Text>{this.state.cartesTirees.length}</Text>
+                <Text>{this.state.carteActuelle[0]}{this.state.carteActuelle[1]}</Text>
             </View>
         )
     }
 }
-
-// const styles = StyleSheet.create({
-// });
 
 export default App
