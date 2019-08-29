295dd04f492f6a4288bb87275f45daed670e7483
Erwanleroy <erwanlr56@gmail.com>
2019-08-30 00:09:51 +0200

    can add/delete rules + sentence for each possibility of card

--- Components/Palmier/theGame.js
+++ Components/Palmier/theGame.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
-
+import { MaterialDialog } from 'react-native-material-dialog';
+import { Input } from 'react-native-elements';
+import { ListItem } from 'react-native-material-ui'
 const NUMEROCARTES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 const FORMESCARTES = ["trefle", "pique", "coeur", "carreau"]
 
@@ -15,6 +17,11 @@ class App extends React.Component {
             cartesRestantes: [],
             cartesTirees: [],
             carteActuelle: [],
+            gameSentence: '',
+            mult: 1,
+            rules: [],
+            newRuleDialog: false,
+            newRuleTmp: '',
         }
     }
 
@@ -35,7 +42,7 @@ class App extends React.Component {
         }
     }
 
-    randomInt = (e) => Math.floor(Math.random() * Math.floor(e));
+    randomInt = e => Math.floor(Math.random() * Math.floor(e));
 
     pickRandomCard = () => {
         if (this.state.cartesRestantes.length !== 0) {
@@ -43,9 +50,56 @@ class App extends React.Component {
             var carteActuelle = this.state.cartesRestantes[idCard]
             this.state.cartesRestantes.splice(idCard, 1)
             this.state.cartesTirees.push(carteActuelle)
-            this.setState({ carteActuelle: carteActuelle })
+            this.setState({ carteActuelle: carteActuelle }, () => this.handleGameSentence())
         } else {
             alert("Plus de cartes deso pas deso")
+        }
+    }
+
+    handleGameSentence = () => {
+        var sentence
+        var carte = this.state.carteActuelle
+        if (carte[0] < 11) {
+            if (carte[1] === "coeur" || carte[1] === "carreau") {
+                sentence = "Tu donnes "
+            } else {
+                sentence = "Tu bois "
+            }
+            sentence += carte[0] * this.state.mult + " gorgees"
+            this.setState({ mult: 1 })
+        } else if (carte[0] === 11) {
+            sentence = "Les anciennes regles sont abandonnees"
+            this.setState({ rules: [] })
+        } else if (carte[0] === 12) {
+            sentence = "Vous pouvez creer une regle"
+            this.newRule(0)
+        } else if (carte[0] === 13) {
+            sentence = "Prochain tour c'est x" + (this.state.mult + 1)
+            this.setState({ mult: this.state.mult + 1 })
+        }
+        this.setState({ gameSentence: sentence })
+    }
+
+
+    newRule = (e) => {
+        // e===0 open prompt box
+        // e===1 add the rule
+        switch (e) {
+            case 0:
+                this.setState({ newRuleDialog: true })
+                break;
+            case 1:
+                var actualRules = this.state.rules
+                actualRules.push(this.state.newRuleTmp)
+                this.setState({
+                    newRuleDialog: false,
+                    newRuleTmp: '',
+                    rules: actualRules
+                }, () => { console.log(this.state.rules) })
+                break;
+            default:
+                alert("wtf")
+                break;
         }
     }
 
@@ -61,7 +115,6 @@ class App extends React.Component {
     render() {
         return (
             <View>
-
                 {/* print All players with them color */}
                 <ScrollView
                     style={{
@@ -103,9 +156,45 @@ class App extends React.Component {
                         : null}
                 </ScrollView>
                 <Button title="Play" onPress={this.play} />
-                <Text style={{ fontSize: 20, textAlign: "center", color: this.state.playerColors[this.state.whoPlay] }}>{this.state.playerNames[this.state.whoPlay]}</Text>
-                <Text>{this.state.cartesTirees.length}</Text>
+                <Text
+                    style={{
+                        fontSize: 20,
+                        textAlign: "center",
+                        color: this.state.playerColors[this.state.whoPlay]
+
+                    }}
+                >
+                    {this.state.playerNames[this.state.whoPlay]}
+                </Text>
+                <Text>cartes restantes: {this.state.cartesRestantes.length}</Text>
                 <Text>{this.state.carteActuelle[0]}{this.state.carteActuelle[1]}</Text>
+                <Text>{this.state.gameSentence}</Text>
+                <MaterialDialog
+                    title="Ajouter une nouvelle regle :"
+                    visible={this.state.newRuleDialog}
+                    onOk={() => this.newRule(1)}
+                    cancelLabel=""
+                    onCancel={() => this.setState({ newRuleDialog: true })}
+                >
+                    <Input
+                        value={this.state.newRuleTmp}
+                        label="Add Rule"
+                        onChangeText={(text) => this.setState({ newRuleTmp: text })}
+                    />
+                </MaterialDialog>
+                {this.state.rules !== [] || this.state.rules !== undefined ?
+                    this.state.rules.map((value, index) => {
+                        return (
+                            <ListItem
+                                key={index}
+                                divider
+                                centerElement={{
+                                    primaryText: value,
+                                }}
+                            />
+                        )
+                    })
+                    : null}
             </View>
         )
     }
